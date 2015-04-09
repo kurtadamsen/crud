87ffa051ff5d8d534d98020f74d3c20cd2749a14
kurtadamsen <kurt.adamsen@gmail.com>
2015-04-09 11:28:40 +0200

    dependency updates

--- project.clj
+++ project.clj
@@ -3,13 +3,13 @@
   :url "http://example.com/FIXME"
 
 
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.1.6"]
                  [org.clojure/java.jdbc "0.3.0-alpha5"]
                  [mysql/mysql-connector-java "5.1.25"]
                  [ring-basic-authentication "1.0.2"]
                  [hiccup "1.0.5"]
-                 [clj-tagsoup "0.3.0"]]
+                 [clj-tagsoup "0.3.0" :exclusions [org.clojure/clojure]]]
   :plugins [[lein-ring "0.8.5"]]
   :user {:plugins [[org.clojars.hozumi/hiccup-bridge "1.0.0-SNAPSHOT"]]}
   :ring {:handler crud.handler/app}
