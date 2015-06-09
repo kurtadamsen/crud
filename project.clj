0b53614dce66b47c5a2be3ce5681ebd24db9ad3c
kurtadamsen <kurt.adamsen@gmail.com>
2015-06-09 15:43:07 +0200

    dependencies

--- project.clj
+++ project.clj
@@ -1,16 +1,16 @@
-(defproject crud "0.1.0"
-  :description "FIXME: write description"
+(defproject crud "0.1.1"
+  :description "Demo and sandbox"
   :url "http://example.com/FIXME"
 
 
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [compojure "1.3.3"]
+                 [compojure "1.3.4"]
                  [org.clojure/java.jdbc "0.3.0-alpha5"]
                  [mysql/mysql-connector-java "5.1.35"]
                  [ring-basic-authentication "1.0.5"]
                  [ring/ring-core "1.3.2"]
                  [ring/ring-json "0.3.1"]
-                 [ring/ring-defaults "0.1.4"]
+                 [ring/ring-defaults "0.1.5"]
                  [hiccup "1.0.5"]
                  [clj-tagsoup "0.3.0" :exclusions [org.clojure/clojure]]]
   :plugins [[lein-ring "0.8.5"]]
